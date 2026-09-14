6f6635ff6759904089a07345daa9f3541b97d4f1
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-09-14 20:14:27 -0300

    cirando os arquivos FastAPI, utilizando o GET para buscar arquivos no backend.

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Cardapio Digital")
 
+cardapio = [
+    {"id": 1, "nome": "Pizza Marguerita", "preco": 39.90, "categoria": "prato"},
+    {"id": 2, "nome": "Suco de Laranja",  "preco": 12.50, "categoria": "bebida"},
+]
+
+@app.get("/itens")
+def listar_itens():
+    return cardapio
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
